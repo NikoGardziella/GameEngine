f922cd0b879ad4579d7793a90809fb6e46744447
NikoGardziella <gardziella@gmail.com>
2023-01-02 21:57:49 +0200

    layers, not finished

---
+++ GameEngine/src/GameEngine/LayerStack.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "GameEngine/Core.h"
+#include "Layer.h"
+
+#include <vector>
+
+namespace GameEngine {
+
+	class GE_API LayerStack
+	{
+	public:
+		LayerStack();
+		~LayerStack();
+
+
+	};
+}
+
