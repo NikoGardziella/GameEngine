4e473e3082a015b68c224fd48ea054d9e1c2d892
Administrator <ngardzie@student.hive.fi>
2023-01-18 13:15:13 +0200

    added layout for vertex buffer

--- GameEngine/src/GameEngine/Application.cpp
+++ GameEngine/src/GameEngine/Application.cpp
@@ -6,13 +6,33 @@
 
 #include "Input.h"
 
-#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
 
 namespace GameEngine {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
 
 	Application* Application::s_Instance = nullptr;
-		
+
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch(type)
+			{
+				case GameEngine::ShaderDataType::Float:  	return GL_FLOAT;
+				case GameEngine::ShaderDataType::Float2: 	return GL_FLOAT;
+				case GameEngine::ShaderDataType::Float3: 	return GL_FLOAT;
+				case GameEngine::ShaderDataType::Float4:  	return GL_FLOAT;
+				case GameEngine::ShaderDataType::Mat3:  	return GL_FLOAT;;
+				case GameEngine::ShaderDataType::Mat4:  	return GL_FLOAT;;
+				case GameEngine::ShaderDataType::Int:  		GL_INT;
+				case GameEngine::ShaderDataType::Int2:  	GL_INT;
+				case GameEngine::ShaderDataType::Int3:  	GL_INT;
+				case GameEngine::ShaderDataType::Int4: 		GL_INT;
+				case GameEngine::ShaderDataType::Bool:  	GL_BOOL;
+			}
+			GE_CORE_ASSERT(false, "Unkown ShaderDatatype!");
+			return 0;
+	}
+
 	Application::Application()
 	{
 		GE_CORE_ASSERT(!s_Instance, "Application already exists!");
@@ -26,16 +46,37 @@ namespace GameEngine {
 		glGenVertexArrays(1, &m_VertexArray);
 		glBindVertexArray(m_VertexArray);		
 
-		float vertices[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			0.5f, -0.5f, 0.0f,
-			0.0f, 0.5f, 0.0f
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		{
+			BufferLayout layout = {
+				{ ShaderDataType::Float3, "a_Position" },
+				{ ShaderDataType::Float4, "a_Color" }
+			};
+			m_VertexBuffer->SetLayout(layout);
+		}
+
+		//BufferLayout layout2(layout);
+
+		uint32_t index = 0;
+		const &auto layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index,
+			GetComponentCount(),
+			ShaderDataTypeToOpenGLBaseType(element.Type),
+			element.Normalized ? GL_TRUE : GL_FALSE,
+			layout.GetStride(),
+			(const void*)element.Offset);
+			index++;
+		}
 
 		unsigned int indices[3] = { 0, 1, 2};
 		m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
@@ -44,10 +85,14 @@ namespace GameEngine {
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
 
+			out vec3 v_Position;
+			out vec4 v_Color;
 
 			void main()
 			{
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position, 1.0);
 			}
 
@@ -58,11 +103,13 @@ namespace GameEngine {
 			
 			layout(location = 0) out vec4 color;
 
-			
+			in vec3 v_Position;
+			in vec4 v_Color;
 
 			void main()
 			{
 				color = vec4(0.8, 0.2, 0.3, 1.0f);
+				color = v_Color;
 			}
 
 		)";
--- GameEngine/src/Platform/OpenGL/OpenGLBuffer.h
+++ GameEngine/src/Platform/OpenGL/OpenGLBuffer.h
@@ -10,10 +10,14 @@ namespace GameEngine {
 		OpenGLVertexBuffer(float* vertices, uint32_t size);
 		virtual ~OpenGLVertexBuffer();
 
-		virtual void Bind() const;
-		virtual void Unbind() const;
+		virtual void Bind() const override;
+		virtual void Unbind() const override;
+
+		virtual const BufferLayout& Getlayout() const override { return m_Layout; }
+		virtual void SetLayout(const BufferLayout& layout) override { m_Layout = layout; }
 	private:
 		uint32_t m_RenderID;
+		BufferLayout m_Layout;
 	};
 
 		class OpenGLIndexBuffer : public IndexBuffer
